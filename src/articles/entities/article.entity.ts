5a37ccdda89ae9e3569d0d89c4c9b2c302d0786f
chenym1992 <18251821296@163.com>
2023-05-11 15:09:11 +0800

    chore: update articles user info

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,3 +1,5 @@
+import { UserEntity } from 'src/users/entities/user.entity';
+
 import { ApiProperty } from '@nestjs/swagger';
 import { Article } from '@prisma/client';
 
@@ -25,4 +27,14 @@ export class ArticleEntity implements Article {
 
   @ApiProperty({ required: false, nullable: true })
   userId: number | null;
+
+  @ApiProperty({ required: false, type: UserEntity })
+  user?: UserEntity;
+
+  constructor({ user, ...data }: Partial<ArticleEntity>) {
+    Object.assign(this, data);
+    if (user) {
+      this.user = new UserEntity(user);
+    }
+  }
 }
